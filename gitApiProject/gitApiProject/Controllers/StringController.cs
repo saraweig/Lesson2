c20d9b9112339da555460c74514f07e76549cde7
וייג שרה < @mbjcomp.local>
2023-12-19 19:30:56 +0200

    creating our controller

---
+++ gitApiProject/gitApiProject/Controllers/StringController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace gitApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StringController : ControllerBase
+    {
+        // GET: api/<StringController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<StringController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<StringController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<StringController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StringController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
